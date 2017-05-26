419da77580238c629bbaaf8069657878a1f5ae1d
FabriGinseng <antoniofabriziofiume@libero.it>
2017-05-26 13:27:13 +0200

    ciao

---
+++ app/src/main/java/com/applicazionediprova/com/myapplication/MainActivityccc.java
@@ -0,0 +1,13 @@
+package com.applicazionediprova.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityccc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityccc);
+    }
+}
